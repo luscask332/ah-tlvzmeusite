7d589c2f0440a4eea038fd072715a854f36fcce9
luscask332 <139006313+luscask332@users.noreply.github.com>
2023-08-11 21:24:38 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Lógica do botão de compartilhar
-const shareButton = document.querySelector('.share-button');
-shareButton.addEventListener('click', function() {
-    const poem = document.querySelector('.poem-container').innerText;
-    const url = window.location.href;
-
-    // Simulando o compartilhamento em uma rede social
-    alert(`Compartilhando o poema:\n${poem}\n\nURL: ${url}`);
-});
